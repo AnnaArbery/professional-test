e73800912cb048ab17547fe01895bfabd46b849c
Anna Arbery <anna.arbery@gmail.com>
2024-02-13 18:24:04 +0300

    refactor code

--- src/components/UI/Datepicker/Datepicker.jsx
+++ src/components/UI/Datepicker/Datepicker.jsx
@@ -1,24 +1,23 @@
 import React, {useState, forwardRef} from 'react';
 import DatePicker, { registerLocale } from 'react-datepicker';
 import ru from 'date-fns/locale/ru';
-registerLocale('ru', ru)
+
 import './Datepicker.scss';
+registerLocale('ru', ru)
 
-const Datepicker = ({title, ...props}) => {
-  return (
-    <div className='field'>
-      <label className='field__label'>Дата заполнения</label>
-      <DatePicker
-        locale='ru'
-        dateFormat='dd.MM.yyyy'
-        customInput={<Datepicker.Input />}
-        {...props}
-      />
-    </div>
-  );
-};
+const Datepicker = (props) => (
+  <div className='field'>
+    <label className='field__label'>Дата заполнения</label>
+    <DatePicker
+      locale='ru'
+      dateFormat='dd.MM.yyyy'
+      customInput={<Datepicker.Input />}
+      {...props}
+    />
+  </div>
+);
 
-Datepicker.Input = React.forwardRef(({ ...props }, ref) => (
+Datepicker.Input = React.forwardRef(( props, ref) => (
   <input
     ref={ref}
     {...props}
